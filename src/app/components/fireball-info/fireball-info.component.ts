b8c7c9c7d1b7799a36d6216b5cfe85d7a3c8b710
Catherine Quilley <catherine.quilley@nology.io>
2019-11-12 21:13:19 +0000

    removed extra method from service

--- src/app/components/fireball-info/fireball-info.component.ts
+++ src/app/components/fireball-info/fireball-info.component.ts
@@ -58,7 +58,7 @@ export class FireballInfoComponent implements OnInit {
   searchByDate(searchTerm){
     console.log("this is the parent: ")
     console.log(searchTerm)
-    this.FireballDataService.fetchDataByMinDate2(searchTerm).then((data) => {
+    this.FireballDataService.fetchDataByMinDate(searchTerm).then((data) => {
       this.fireballData = data;
       this.countOfFireballs = this.fireballData.length;
       // console.log(this.fireballData);
--- src/app/services/fireball-data.service.ts
+++ src/app/services/fireball-data.service.ts
@@ -15,15 +15,8 @@ export class FireballDataService {
     });
   }
 
-  public fetchDataByMinDate() {
-    return this.http.get('https://ssd-api.jpl.nasa.gov/fireball.api?date-min=2019-10-01&req-loc=true')
-      .toPromise()
-      //.then(data => data)
-      .then(data => this.hydrateFireballs(data))
-      .catch()
-  }
-
-  public fetchDataByMinDate2(date) {
+  
+  public fetchDataByMinDate(date) {
     return this.http.get(`https://ssd-api.jpl.nasa.gov/fireball.api?date-min=${date}&req-loc=true`)
       .toPromise()
       //.then(data => data)
